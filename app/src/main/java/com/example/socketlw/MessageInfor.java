13964ea2ea67130cd87fec630a3d55fdce379bfd
22861 <xiao2023345005@gmail.com>
2024-09-21 09:38:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/socketlw/MessageInfor.java
@@ -0,0 +1,61 @@
+package com.example.socketlw;
+public class MessageInfor {
+    private int ID;
+    private String msg;
+    private Long time;
+    private Long userID;
+    /**
+     * 作者 LinOwl
+     * 2021.02.17
+     */
+    public MessageInfor(String msg, Long time, Long userID, String type) {
+        this.msg = msg;
+        this.time = time;
+        this.userID = userID;
+        this.type = type;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+
+
+}
